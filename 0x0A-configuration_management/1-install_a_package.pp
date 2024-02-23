96943db70bba1c276d1a40e26796a6c1d5915537
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2024-02-23 23:37:13 +0100

    trying to solve lint error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# installs flask from pip3
 #!/usr/bin/pup
-
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
