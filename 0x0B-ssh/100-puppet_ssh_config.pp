8ac094b1f3dfdabaa85b014c21f54ac19cc11a0b
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2024-02-24 12:47:24 +0100

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# changes the SSH client configuration using Puppet
+
+exec { 'echo':
+path     => 'usr/bin:/bin',
+command  => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+returns  => [0,1],
+}
